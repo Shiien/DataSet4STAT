91cc64da380579bf4afa1db5ef1445713248186a
SaucerHi <935697492@qq.com>
2026-04-30 21:25:53 +0800

    Bug fixed. Thanks for Zijun!

--- low_rank_regression.py
+++ low_rank_regression.py
@@ -3,38 +3,41 @@ import jax.numpy as jnp
 import jax
 import distrax
 import optax
+import chex
 
 def low_rank_model(D,K):
-
     alpha=2.0
     beta=2.0
     U = distrax.Laplace(0.0,1.0)._sample_n(key=jax.random.PRNGKey(72), n=D)
+    U = U.reshape((D,1))
     A = U@U.T
-    b = distrax.Gamma(2.0,2.0).sample(seed=jax.random.PRNGKey(72)) * jnp.ones([D])
-
+    
+    b = distrax.Gamma(2.0,2.0).sample(seed=jax.random.PRNGKey(72)) * jnp.ones([D, 1])
+    b = b.reshape((D,1))
+    chex.assert_shape(A,(D, D))
+    chex.assert_shape(U,(D,1))
+    chex.assert_shape(b,(D, 1))
     @jax.vmap
     def log_pdf(x, y):
-        return distrax.Bernoulli(jax.nn.sigmoid(jnp.sum(x*U * U*b,axis=-1))).log_prob(y)
-    def sample(prng,bsz):
+        chex.assert_shape(x.T@U@U.T@b,(1,))
+        chex.assert_shape(y,(1,))
+        return jnp.squeeze(distrax.Bernoulli(jax.nn.sigmoid(x.T@U@U.T@b)).log_prob(y))
+    def sample(prng, bsz):
         prng, subprng = jax.random.split(prng)
         x = distrax.Normal(0.0,1.0)._sample_n(subprng, bsz*D)
-        x = x.reshape((bsz, -1))
-        prng, subprng = jax.random.split(prng)
-        y = distrax.Bernoulli(jax.nn.sigmoid(jnp.sum(x*U * U*b,axis=-1))).sample(seed=subprng)
-        return x,y
+        x = x.reshape((bsz, D, 1))
+        subprng = jax.random.split(prng, bsz)
+        y = jax.vmap(lambda _x, pr: distrax.Bernoulli(jax.nn.sigmoid(_x.T@U@U.T@b)).sample(seed=pr), in_axes=(0,0))(x, subprng)
+        return jnp.squeeze(x,axis=-1),jnp.squeeze(y, axis=-1)
     return log_pdf, sample
 
 logpdf, sample = low_rank_model(100,1)
 prng = jax.random.PRNGKey(42)
 x,y = sample(prng, 1000)
 import matplotlib.pyplot as plt  
-# print(x.shape,y.shape)
-# print(x,y,logpdf(x,y))
 
-import numpy
 import pandas as pd
-print(x.shape,y.shape)
-x = jnp.concatenate([x,y.reshape((1000,1))],axis=-1)
+x = jnp.concatenate([x,y],axis=-1)
 my_df = pd.DataFrame(np.asarray(x))
 my_df.columns = [f'x{i}' for i in range(100)]+['y']
 my_df.to_csv('regression.csv', index=False)
